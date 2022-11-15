a22aaa1a5ea489897bb8a1fee98c528de2b46894
Kevinas Ramoska <KEVINAS.RAMOSKA.2021@MUMAIL.IE>
2022-11-15 13:50:38 +0000

    good stuff

--- cs264lab3/Program.cs
+++ cs264lab3/Program.cs
@@ -174,7 +174,7 @@ namespace MyNamespace
     public class Memento
     {
         public string shape;
-        Memento(string data)
+        public Memento(string data)
         {
             this.shape=data;
         }
@@ -184,7 +184,7 @@ namespace MyNamespace
     }
     // https://www.letsdevelopgames.com/2022/01/memento-design-pattern-for-undoredo.html?m=1
     // This is caretaker class
-        class canvas{
+        public class canvas{
             List<Memento> undoStates = new List<Memento>();
             List<Memento> redoHistory = new List<Memento>();
             public canvas(){
@@ -201,13 +201,15 @@ namespace MyNamespace
             }
             //Add
             public void addMenento(Memento newshape){
-                shape.Add(newshape);
+                undoStates.Add(newshape);
             }
             //Undo
-            void removeMenento()
+            public void removeMenento()
             {
-                Memento redoItem = this.undoStates.Pop();
-                this.redoHistory.Add(redoItem);
+                if(undoStates.Count > 0){
+                    redoHistory.Add(undoStates[undoStates.Count - 1]);
+                    undoStates.RemoveAt(undoStates.Count - 1);
+                }
             }
             //Get Mememto
             public Memento getMemento(int index){
@@ -217,12 +219,16 @@ namespace MyNamespace
             {
                 return this.undoStates;
             }
+            public void clearCanvas(){
+                undoStates.Clear();
+                redoHistory.Clear();
+            }
         }
         //This is the originator class
         static void Main(string[] args)
         {
             canvas canvas1 = new canvas();
-            Console.WriteLine( canvas.getUndoStates());
+            Console.WriteLine( canvas1.getUndoStates());
            
             
             string svgOpen = @"<svg height=""400"" width=""400"" xmlns=""http://www.w3.org/2000/svg"">" + Environment.NewLine;
@@ -241,28 +247,28 @@ namespace MyNamespace
                 Console.WriteLine("Canvas created - use commands to add shapes to canvas");
                 
                 char command = char.Parse(Console.ReadLine());
-                if(command == 'H'){
+                if(command == 'H' || command == 'h'){
                     Console.WriteLine("Commands: H	 	 Help	-	displays	this	message  \nA	<shape>	 Add	<shape	to	canvas	  \n D   - Display Canvas \nU	 	 Undo	last	operation	 \n R	 	 Redo	last	operation	 \n C	 	 Clear	canvas	 \n Q	 	 Quit	application");
                 }
-                else if(command == 'A'){
+                else if(command == 'A' || command == 'a'){
                     //Add shape
                     Console.WriteLine("Enter shape type: R for rectangle, C for circle, L for line, P for polygon, S for polyline, or T for path");
                     char shapeType = char.Parse(Console.ReadLine());
-                    if(shapeType == 'R'){
+                    if(shapeType == 'R'  || shapeType == 'r'){
                         //Rectangle
-                        Console.WriteLine("Enter x coordinate");
-                        int x = int.Parse(Console.ReadLine());
-                        Console.WriteLine("Enter y coordinate");
-                        int y = int.Parse(Console.ReadLine());
-                        Console.WriteLine("Enter width");
-                        int width = int.Parse(Console.ReadLine());
-                        Console.WriteLine("Enter height");
-                        int height = int.Parse(Console.ReadLine());
-                        Rectangle rectangle = new Rectangle(x, y, width);
-                        canvas1.addMenento(rectangle.ToString());
-                        Console.WriteLine("Rectangle added to canvas");
+                        // Console.WriteLine("Enter x coordinate");
+                        // int x = int.Parse(Console.ReadLine());
+                        // Console.WriteLine("Enter y coordinate");
+                        // int y = int.Parse(Console.ReadLine());
+                        // Console.WriteLine("Enter width");
+                        // int width = int.Parse(Console.ReadLine());
+                        // Console.WriteLine("Enter height");
+                        // int height = int.Parse(Console.ReadLine());
+                        // Rectangle rectangle = new Rectangle(x, y, width);
+                        // canvas1.addMenento(new Memento(rectangle.ToString()));
+                        // Console.WriteLine("Rectangle added to canvas");
                     }
-                    else if(shapeType == 'C'){
+                    else if(shapeType == 'C' || shapeType == 'c'){
                         //Circle
                         Console.WriteLine("Enter x coordinate");
                         int x = int.Parse(Console.ReadLine());
@@ -272,10 +278,10 @@ namespace MyNamespace
                         int radius = int.Parse(Console.ReadLine());
                         Circle circle = new Circle(x, y, radius);
 
-                        canvas1.addMenento(circle.ToString());
+                        canvas1.addMenento(new Memento(circle.ToString()));
                         Console.WriteLine("Circle added to canvas");
                     }
-                    else if(shapeType == 'L'){
+                    else if(shapeType == 'L' || shapeType == 'l'){
                         //Line
                         Console.WriteLine("Enter x1 coordinate");
                         int x1 = int.Parse(Console.ReadLine());
@@ -286,37 +292,48 @@ namespace MyNamespace
                         Console.WriteLine("Enter y2 coordinate");
                         int y2 = int.Parse(Console.ReadLine());
                         Line line = new Line(x1, y1, x2, y2);
-                        canvas1.addMenento(line.ToString());
+                         canvas1.addMenento(new Memento(line.ToString()));
                         Console.WriteLine("Line added to canvas");
                     }
 
                 }
-                else if(command == 'U'){
+                else if(command == 'U' || command == 'u'){
                     //undo last operation
                     canvas1.removeMenento();
                     Console.WriteLine("Undo last operation");
                 }
-                else if(command == 'D'){
-                        for(int i = 0; i < canvas1.getUndoStates().Count; i++){
-                        Console.WriteLine(canvas1.getUndoStates()[i]);
-                        }
+                else if(command == 'D' || command == 'd'){
+                       //Display Canvas
+                    Console.WriteLine("Display Canvas");
+                    for (int i = 0; i < canvas1.getUndoStates().Count; i++)
+                    {
+                        Console.WriteLine(canvas1.getUndoStates()[i].getData());
                     }
+                }
                 
-                else if(command == 'R'){
+                else if(command == 'R' || command == 'r'){
                     //Redo last operation
                     canvas1.redo();
                     Console.WriteLine("Redo last operation");
                 }
-                else if(command == 'C'){
+                else if(command == 'C' || command == 'c'){
                     //Clear canvas
-                    canvas.Clear();
+                    canvas1.clearCanvas();
                 }
-                else if(command == 'S'){
+                else if(command == 'S' || command == 's'){
                     //Save canvas
                     Console.WriteLine("Save canvas");
-
+                    string svg = svgOpen;
+                    for (int i = 0; i < canvas1.getUndoStates().Count; i++)
+                    {
+                        svg += canvas1.getUndoStates()[i].getData();
+                    }
+                    svg += svgClose;
+                    Console.WriteLine(svg);
+                    File.WriteAllText("canvas.svg", svg);
                 }
-                else if(command == 'Q'){
+                else if(command == 'Q' || command == 'q'){
+                    //Quit application
                        break;
                        Console.WriteLine("Quit application");
                 }
@@ -337,20 +354,16 @@ namespace MyNamespace
                 // Console.WriteLine("14): Undo");
                 // int choice = int.Parse(Console.ReadLine());
                 //Create Rectangle
-                if (choice == 1)
-                {
-                    //create rectangle
-                    Console.WriteLine("Enter x coordinate: ");
-                    int x = int.Parse(Console.ReadLine());
-                    Console.WriteLine("Enter y coordinate: ");
-                    int y = int.Parse(Console.ReadLine());
-                    Rectangle rect = new Rectangle(x, y);
-                    canvas.Add(rect.ToString());
-                    Console.WriteLine(rect.ToString());
-                    Memento memento = new Memento(rect.ToString);
- 
-                    canvas1.addMenento(new Memento(rect.ToString()));
-                }
+                // if (choice == 1)
+                // {
+                //     //create rectangle
+                //     Console.WriteLine("Enter x coordinate: ");
+                //     int x = int.Parse(Console.ReadLine());
+                //     Console.WriteLine("Enter y coordinate: ");
+                //     int y = int.Parse(Console.ReadLine());
+                //     Rectangle rect = new Rectangle(x, y);
+                //     canvas1.addMenento(new Memento(rect.ToString()));
+                // }
             //     //Create Circle
             //     else if (choice == 2)
             //     {
